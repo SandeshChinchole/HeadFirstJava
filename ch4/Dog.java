130a73b03e6e43ccaf7a4f39fe9bd5268f92a951
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-03-22 10:19:28 -0400

    added files in ch4

---
+++ ch4/Dog.java
@@ -0,0 +1,18 @@
+
+public class Dog {
+
+	String name;
+	int size;
+	
+	void bark() {
+		if(size > 60) {
+			System.out.println("Woof! Woof!");
+		}
+		else if(size > 14) {
+			System.out.println("Ruff! Ruff!");
+		}
+		else {
+			System.out.println("Yipp! Yipp!");
+		}
+	}
+}
---
+++ ch4/DogTestDrive.java
@@ -0,0 +1,19 @@
+
+public class DogTestDrive {
+
+	public static void main(String[] args) {
+
+		Dog one = new Dog();
+		one.size = 70;
+		one.bark();
+		
+		Dog two = new Dog();
+		two.size = 8;
+		two.bark();
+		
+		Dog three = new Dog();
+		three.size = 35;
+		three.bark();
+	}
+
+}
