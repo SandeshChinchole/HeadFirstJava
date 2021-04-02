62d95ec8da0e55047d98128d920cb2f89b78a693
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-04-02 10:49:53 -0400

    added files in ch7

---
+++ ch7/Animal.java
@@ -0,0 +1,26 @@
+
+public class Animal {
+
+	String picture;
+	String food;
+	String location;
+	String hunger;
+	String boundaries;
+	
+	public void makeNoise() {
+		
+	}
+	
+	public void eat() {
+		
+	}
+	
+	public void sleep() {
+		
+	}
+	
+	public void roam() {
+		
+	}
+
+}
---
+++ ch7/AnimalTester.java
@@ -0,0 +1,17 @@
+
+public class AnimalTester {
+
+	public static void main(String[] args) {
+
+		Animal[] animals = new Animal[2];
+		
+		animals[0] = new Dog();
+		animals[1] = new Cat();
+		
+		for(int i = 0; i < animals.length; i++) {
+			
+			animals[i].makeNoise();
+		}
+	}
+
+}
